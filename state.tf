4bf28d6a29bfa32a2a0399cb0b26f62a93028560
matheuscg <matheuscg@ymail.com>
2021-05-26 19:06:52 -0300

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackaton-fiap-5dvp-338893"
+    bucket = "hackathon-fiap-5dvp-338893"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
